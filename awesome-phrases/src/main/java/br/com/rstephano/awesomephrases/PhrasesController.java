f244846059417cc50d518e04a235c57ab54a129e
Rafael Stephano <servicoiteris732661@userede.com.br>
2019-06-26 14:07:59 -0300

    Implementação de service-discovery e eureka

---
+++ awesome-phrases/src/main/java/br/com/rstephano/awesomephrases/PhrasesController.java
@@ -0,0 +1,26 @@
+package br.com.rstephano.awesomephrases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.discovery.EurekaClient;
+
+@RestController
+public class PhrasesController {
+
+  @Autowired
+  @Lazy
+  private EurekaClient eurekaClient;
+
+  @Value("${spring.application.name}")
+  private String appName;
+
+  @RequestMapping("/greeting")
+  String greeting() {
+    return String.format("Hello from '%s'!", eurekaClient.getApplication(appName).getName());
+  }
+
+}
